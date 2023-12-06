a1f5ab51607699ab2d1723875b1d4cc6a62b0488
John Law <john.law@dexibit.com>
2023-12-06 17:40:04 +1300

    Update example for repeated queries

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("org.duckdb:duckdb_jdbc:0.8.1")
+    implementation("org.duckdb:duckdb_jdbc:0.9.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
+    implementation("com.zaxxer:HikariCP:5.0.1")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,30 @@
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.sql.DriverManager
-import java.sql.Statement
-import java.util.*
+import com.zaxxer.hikari.HikariDataSource
+import java.util.concurrent.TimeUnit
 
 
 fun main(args: Array<String>) {
+    val token = System.getenv("MD_TOKEN")
+    println("started")
+    while (true){
+        val ds = HikariDataSource()
+        ds.setJdbcUrl("jdbc:duckdb:md:file_db?token=${token}")
+        val conn = ds.getConnection()
 
-    // Class.forName("org.duckdb.DuckDBDriver");
-    // val ro_prop = Properties()
-    // ro_prop.setProperty("duckdb.read_only", "true")
-    val conn = DriverManager.getConnection("jdbc:duckdb:md:file_db?token=")
-    println("==== complete ====")
+        try {
+
+            println("")
+            println("======== Query ========")
+            // Execute SHOW TABLES with conn
+            val stmt = conn.createStatement()
+            val result = stmt.executeQuery("SELECT review, location FROM john_test_review_x LIMIT 1")
+            while (result.next()) {
+                println(result.getString(1))
+            }
+            println("======== success ========")
+        } catch (e: Exception) {
+            println(e.message)
+            println("======== failed ========")
+        }
+        TimeUnit.SECONDS.sleep(3)
+    }
 }
